e0ec8a86099abb4dccd4a61fa691c610b4e9647b
Henry Yang <contacthenryyang@gmail.com>
2018-12-04 23:56:17 -0800

    Upgraded temporary source image

--- client/statusTable.jsx
+++ client/statusTable.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from '../public/kizenLogo.png';
-// need to install image loader
+// import logo from '../public/kizenLogo.png';
 
 class StatusTable extends React.Component {
   constructor(props) {
@@ -14,7 +13,8 @@ class StatusTable extends React.Component {
   render () {
     return (
       <div>
-        <img src={logo} alt="Logo"></img>
+        <img src="https://static1.squarespace.com/static/5b078144f8370af3f2b20e0b/t/5b0e075d575d1f5bd35fdc33/1527646049706/Screen+Shot+2018-05-29+at+10.06.58+PM.png?format=750w" alt="Logo"></img>
+        {/* temporary 3rd party logo, need to install imageloader later for webpack */}
         <h1>{this.state.quote}</h1>
       </div>
     );
